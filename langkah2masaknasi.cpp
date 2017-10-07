8e194bf745a94575c0403ecb25d49b518aaca518
dian95 <32361422+dian95@users.noreply.github.com>
2017-10-07 11:57:13 +0700

    Add files via upload

---
+++ langkah2masaknasi.cpp
@@ -0,0 +1,24 @@
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+ 
+cout<<"langkah langkah memasak nasi di rice cooker"<<endl;
+cout<<"1. ambil wadah"<<endl;
+cout<<"2. masukan air kedalam wadah tersebut"<<endl;
+cout<<"3. masukan beras ke dalam wadah tersebut"<<endl;
+cout<<"4. cuci beras di dalam wadah tersebut"<<endl;
+cout<<"5. buang air hasil cucian beras tersebut dari wadah "<<endl;
+cout<<"6. masukan lagi air bersih kedalam wadah tersebut secukupnya"<<endl;
+cout<<"7. masukan wadah tersebut ke dalam rice cooker"<<endl;
+cout<<"8. colokan rice cooker ke terminal( arus listrik)"<<endl;
+cout<<"9. ON kan rice cooker dengan cara menekan tombol on ke bawah"<<endl;
+cout<<"10. tunggu sampai tombol on nyetrek lagi ke atas "<<endl;
+cout<<"11. nasi siap di dihidangkan"<<endl;
+  
+
+  return 0;
+}
