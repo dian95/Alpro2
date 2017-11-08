9a3273b6563cdb004a9632a4be2787b7a2c55b74
DIAN RAMDANI <32361422+dian95@users.noreply.github.com>
2017-11-08 18:01:30 +0700

    Add files via upload

---
+++ dian.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+using namespace std;
+
+
+
+int main(){
+	int x=3;
+	string nama1 [x];
+
+	for(int i = 0 ; i<x ; ++i)
+	{
+	cin >> nama1 [i];
+	}
+		for(int i = 0 ; i<x ; ++i)
+	{
+	cout << nama1 [i]<<endl;
+	}
+
+	return 0;
+}
+
+
+
