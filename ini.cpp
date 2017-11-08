c614562b5b566cb40108b057b27748fb7b430e5a
DIAN RAMDANI <32361422+dian95@users.noreply.github.com>
2017-11-08 18:00:28 +0700

    Add files via upload

---
+++ ini.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+using namespace std;
+
+
+
+int main(){
+	int x=10;
+	string nama1 [x];
+	string nama2 [10];
+
+	nama1 [0] = "frencius";
+	nama2 [0] = "frencius";
+
+	cout << nama1 [0]<<endl;
+	cout << nama2 [0];
+
+
+	return 0;
+}
+
