0e897b9ecae415b97a91440c76fc40ab8710dcfd
DIAN RAMDANI <32361422+dian95@users.noreply.github.com>
2017-11-28 21:53:54 +0700

    Add files via upload

---
+++ latihan aray.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	string nama [10];
+ 
+for (int i=0;i<10;++i)
+	{
+			cin >> nama [i];
+	}
+		cout<< endl;
+
+for (int i=0;i<10;++i)
+	{
+			cout << nama [i];
+			cout<< endl;
+	}
+    return 0;
+}
